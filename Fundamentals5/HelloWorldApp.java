5522c623579f69f180c320597daddd6d489cac54
cmis <cmis@cmis-hs02.(none)>
2015-08-24 09:55:17 +0700

    Starting Fundamentals5

--- Fundamentals5/HelloWorldApp.java
+++ Fundamentals5/HelloWorldApp.java
@@ -3,7 +3,13 @@ public class HelloWorldApp {
   public static void main ( String args[] )
   {
    
+    str[] movies = {Lord of the Rings, Belle, Into the Rough };
+    str[] songs = {Demons, The Scientist, Chandelier};
     
+    System.out.println("My three favoriet movies are:" + nums.length ):
+    
+      
+      
     for( int ctr = 0; ctr < 10; ctr++ )
     {
         System.out.println( ctr + "\t" + ctr*ctr + "\t" + (int)(ctr + (ctr * Math.random()) ));
