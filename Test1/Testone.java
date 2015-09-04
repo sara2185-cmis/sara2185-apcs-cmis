b71938464ee7c27d70958f88cb7ba92867fe83e5
cmis <cmis@cmis-hs02.(none)>
2015-09-04 11:57:12 +0700

    Finished Testing

--- Test1/Testone.java
+++ Test1/Testone.java
@@ -7,34 +7,34 @@ public class Testone {
     String input = JOptionPane.showInputDialog( "Enter a number.");
     int length = Integer.parseInt( input );
     int array[] = new int [length];
-     
-    for( int index = 0; index < array.length; index++ )
+    double nums = ((Math.random() * 51));// I was trying to use this as a variable to put in the for loop
+    
+    for( int index = 0; index < array.length; index++ ) // creating random number
     {
           array[index] = ((int)(Math.random() * 51));
         
     } // end for loop
-    for( int index = 0; index < array.length; index++)
+    for( int index = 0; index < array.length; index++) // printing array of random numbers
     {
         System.out.println(index + ":" + array[index]);
     } // end for loop
    
     for( int index = array.length-1; index > 0; index-- )
     {
-        int even = array[index];
-        if( array[index] % 2 == 0)
+
+        if( array[index] % 2 == 0)// even number
         {
-            
+         
           System.out.println(index + ":" + "E" + array[index]);
           
-        } 
+        }// end if 
         
-        else
+        else// odd number
         {
            System.out.println(index + ":" + "O" + array[index]);
-         }
+         } // end else
         
     } // end for loop
     
-   
   } // end main
 } // end HelloWorld
