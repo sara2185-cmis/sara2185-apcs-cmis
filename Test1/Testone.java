9280377bf46088e2bd29f732eee301800f001d16
cmis <cmis@cmis-hs02.(none)>
2015-09-04 11:30:37 +0700

    Testing

--- Test1/Testone.java
+++ Test1/Testone.java
@@ -7,21 +7,22 @@ public class Testone {
     String input = JOptionPane.showInputDialog( "Enter a number.");
     int length = Integer.parseInt( input );
     int array[] = new int [length];
-    double random = (50 * (Math.random()));
-    
-     for( int index = 0; index < array.length; index++ )
+     
+    for( float index = 0; index < array.length; index++ )
     {
-           array[index] = (( Math.floor(Math.random() * 51));
+           array[index] = ( Math.floor(Math.random() * 51));
         
     } // end for loop
-    for( int index = 0; index < array.length; index++ )
+    for( float index = 0; index < array.length; index++)
     {
-           System.out.println(array[index] );
+        System.out.println(index + ":" + array[index]);
+    } // end for loop
+   
+    for( float index = array.length-1; index > 0; index-- )
+    {
+           System.out.println(index + ":" + array[index]);
         
     } // end for loop
-    
-    
-    
     
   } // end main
 } // end HelloWorld
