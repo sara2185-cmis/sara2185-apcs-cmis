8e877cd7828a130efc069c5608656e4367a97399
cmis <cmis@cmis-hs01.(none)>
2015-09-16 09:53:57 +0700

    Still doing histogram

--- Histogram/Histogram.java
+++ Histogram/Histogram.java
@@ -8,7 +8,7 @@ public class Histogram
         int num= Integer.parseInt( input1);
         int randomNums[] = new int [num];
         int counter [] = new int [100];
-        for( int index = 0; index < randomNums.length; index++ ) // creating random number
+        for( int index = 0; index < randomNums.length; index++ ) 
         {
             randomNums[index] = (int) ((Math.random() * 100));
             for( int index1 = 0; index1 < 100; index1 ++)
@@ -17,24 +17,48 @@ public class Histogram
                 {
                     counter[index1]++;
                 }//end if
-             
+
             } //end for loop    
         } // end for loop
-        for( int index = 0; index < 100; index++ ) // creating random number
+        for( int index = 0; index < 100; index++ ) 
         {
-            System.out.println(index + " : " + counter[index]);
-
-        } // end for loop
-        int minimumNum = 0;
-        for( int index = randomNums.length; index > 0; index-- ) // creating random number
-        {
-
-            if(randomNums[index-1] < randomNums[index])
-            {
-                randomNums[index] = minimumNum;
-            }// end if statement
-            System.out.println(minimumNum);
+            System.out.print(index + " : ");
+           for( int asterick = 0; asterick < counter[index]; asterick++)
+           {
+             System.out.print("#"); 
+            } // end for loop
+            System.out.println(" ");
+            
         } // end for loop
         
+        /*int minimumNum = 0;
+        for( int index = 0; index < 100; index++ ) // finding minimum number
+        {
+            if (index==99)
+            {
+                if(randomNums[index] < randomNums[index])
+                {
+                    minimumNum = randomNums[index];
+                }// end if statement
+            }
+            else
+            {
+                if(randomNums[index] < randomNums[index+1])
+                {
+                    minimumNum = randomNums[index];
+                }// end if statement
+            }
+            System.out.println(minimumNum);
+        } // end for loop
+        */
+        int sum = 0; 
+        int total = 0;
+        for( int index = 0; index < randomNums.length; index++ ) // finding minimum number
+        {
+            total = randomNums[index] + sum;
+          
+        } // end for loop
+       System.out.println(total);
+       
     } // end main
 } // end HelloWorld
