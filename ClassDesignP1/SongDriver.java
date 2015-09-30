88a4470a59c2727d9f797de7f2d47a5f48bc4e91
cmis <cmis@cmis-hs02.(none)>
2015-09-30 14:33:08 +0700

    Starting Class Design

---
+++ ClassDesignP1/SongDriver.java
@@ -0,0 +1,11 @@
+public class SongDriver
+{
+        public static void main( String[] args )
+        {
+           
+            Song mySong = new Song("Cold Play", "Paradise", 4);
+            Song myOtherSong = new Song();
+            System.out.println( mySong);
+            System.out.println( myOtherSong);
+        }//end method main
+}// end class SongDriver
