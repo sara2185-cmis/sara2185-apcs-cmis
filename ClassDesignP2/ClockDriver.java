d7b837b741b6dc2bf6edf0e3ac626c6241b41564
cmis <cmis@cmis-hs02.(none)>
2015-10-08 14:35:53 +0700

    Still doing class design

--- ClassDesignP2/ClockDriver.java
+++ ClassDesignP2/ClockDriver.java
@@ -3,11 +3,11 @@ public class ClockDriver
 {
     public static void main( String[] args )
     {
-        int inputHour = ("%0d",Integer.parseInt(JOptionPane.showInputDialog("What hour is it?")));
-        int inputMin = ("%0d",Integer.parseInt(JOptionPane.showInputDialog("What minutes is it?"));
-        int inputSec = "%0d",Integer.parseInt(JOptionPane.showInputDialog("What seconds is it?"));
+        int inputHour =( Integer.parseInt(JOptionPane.showInputDialog("What hour is it?")));
+        int inputMin = ( Integer.parseInt(JOptionPane.showInputDialog("What minutes is it?")));
+        int inputSec = ( Integer.parseInt(JOptionPane.showInputDialog("What seconds is it?")));
         
-        Clock bedroomClock = new Clock("%0d",inputHour, "%0d",inputMin, "%0d",inputSec);
+        Clock bedroomClock = new Clock(String.format("%02d",inputHour, "%02d",inputMin, "%02d",inputSec));
         Clock kitchenClock = new Clock();
         System.out.println( bedroomClock);
         System.out.println( kitchenClock);
--- ClassDesignP5/Card.java
+++ ClassDesignP5/Card.java
@@ -4,8 +4,21 @@ public class Card
     //1. Create instance variables
     private String suit;
     private String rank;
-    private int deck = 52;
 
+    public static void main ( String args[] )
+    {
+        String suit[] = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        String rank[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+        Card myHand[] = new Card[3];
+        for(int i = 0; i < myHand.length; i++)
+        {
+            myHand[(int) ( 4* Math.random())] = suit[i];
+            myHand[(int) ( 13* Math.random())] = rank[i];
+            
+        }//end for loop
+        
+    }// end main
+/*
     //2. Write constructor to initialize instance variables
     public Card()
     {
@@ -13,9 +26,9 @@ public class Card
     } // end constructor Song
     public Song(String artist, String name, int length)
     {
-       this.artist = artist;
-       this.name = name;
-       this.length = length;
+        this.artist = artist;
+        this.name = name;
+        this.length = length;
     } // end constructor Song
     //3. toString()
     public String toString()
@@ -26,5 +39,5 @@ public class Card
         "Song length is: " + length;
         return output;
     }
-
-} // end Song
+*/
+} // end Card
