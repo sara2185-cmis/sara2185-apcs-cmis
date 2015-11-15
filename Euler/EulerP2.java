fb7bec9582ee4b749019360036c84440f9cd16f5
Sara Tan <tortorareed@Dharit-Tantiviramanonds-MacBook-Pro.local>
2015-11-15 13:05:50 +0700

    Euler Projects

--- Euler/EulerP2.java
+++ Euler/EulerP2.java
@@ -3,18 +3,15 @@ public class EulerP2
     public static void main( String[] args )
     {
         int value = 0;
-        int one = 1;
-        int two = 1;
-        for(int i = 2; i < 4000000; i++)
+        for(int i = 2; i < 100; i++)
         {
             i = (i-1) + (i-2);
-            while (value < 4000000)
-            {
-            if (i%2 ==0)
-            {
-                value+=i;
-            }
-        }
+            
+                if (i%2 ==0)
+                {
+                    value +=i;
+                }
+            
 
         }//end for loop
         System.out.print(value);
