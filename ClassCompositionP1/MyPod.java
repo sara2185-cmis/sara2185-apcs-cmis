7bf52acdfdf56153482bdf9174678cfce96513e3
Sara Tan <tortorareed@Dharit-Tantiviramanonds-MacBook-Pro.local>
2016-01-17 16:51:17 +0700

    Class Composition work

---
+++ ClassCompositionP1/MyPod.java
@@ -0,0 +1,49 @@
+
+/**
+ * Write a class definition for a MyPod object that stores information about your personal music player. Class MyPod should include the following: 
+an instance variable for its color
+an instance variable for its memory capacity (in GB)
+an instance variable of type array that can hold 3 Song objects called “songLibrary” that represents all songs in the MyPod
+a zero-argument and multi-argument constructor to initialize all instance variables
+a toString() method to display the object’s instance variables in a user friendly format. 
+
+ * 
+ * @Sara
+ * @January 17th, 2016
+ */
+public class MyPod
+{
+    private String color;
+    private int memoryCapacity;
+    private String[] songLibrary = new String[3];
+
+    
+    public MyPod()
+    {
+        color = "Blue";
+        memoryCapacity = 4; //in GB
+        songLibrary[0] = "Dirty Paws";
+        songLibrary[1] = "The Scientist";
+        songLibrary[2] = "Happy";
+        
+        
+    }
+
+    public MyPod(String color, int memoryCapacity, String[] songLibrary)
+    {
+        this.color = color;
+        this.memoryCapacity = memoryCapacity;
+        this.songLibrary = songLibrary;
+    }
+    
+     public String toString()
+    {
+        String output = new String();
+        output = "The color is " + color + ", the memory capacity is " + memoryCapacity + ", and the songs are ";
+        for(String song : songLibrary)
+        {
+            output += song + " ";
+        }
+        return output;
+    }
+}
---
+++ ClassCompositionP1/MyPodDriver.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class MyPodDriver here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyPodDriver
+{
+   MyPod carPod = new MyPod("Green", 8, new String[] {"Hello", "Rolling in the Deep", "Set Fire to the Rain"});
+   System.out.println(carPod.toString());
+}
+
